8e0887c72ea77ab01d5e8a0951fc10c8f997b355
Stéphane B <stillfury@hotmail.com>
2022-01-04 21:42:31 -0500

    feat put project under version control

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+// We need Nodes fs module to read directory contents
+const fs = require('fs');
+
+function generateHtmlPlugins(templateDir) {
+    const dirents = fs.readdirSync(path.resolve(__dirname, templateDir), { withFileTypes: true });
+    const EXTENSION = '.html';
+
+    const filesNames = dirents
+        .filter(dirent => dirent.isFile())
+        .map(dirent => dirent.name)
+        .filter(dirent => path.extname(dirent).toLowerCase() === EXTENSION);
+
+    return filesNames.map(item => {
+        // Split names and extension
+        const parts = item.split('.')
+        const name = parts[0]
+        const extension = parts[1]
+        return new HtmlWebpackPlugin({
+            filename: `${name}.html`,
+            template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+        })
+    })
+}
+
+// We will call the function like this:
+const htmlPlugins = generateHtmlPlugins('./src');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: './src/img', to: 'img'}
+            ],
+        }),
+    ].concat(htmlPlugins),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
