15784208abe06a995501b9f478c579cd6452ecd9
Stéphane B <stillfury@hotmail.com>
2022-02-21 16:20:56 -0500

    build: add environment variable

--- webpack.common.js
+++ webpack.common.js
@@ -1,51 +1,53 @@
-const webpack = require('webpack');
-const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 // We need Nodes fs module to read directory contents
-const fs = require('fs');
+const fs = require("fs");
+const Dotenv = require("dotenv-webpack");
 
 function generateHtmlPlugins(templateDir) {
-    const dirents = fs.readdirSync(path.resolve(__dirname, templateDir), { withFileTypes: true });
-    const EXTENSION = '.html';
+  const dirents = fs.readdirSync(path.resolve(__dirname, templateDir), {
+    withFileTypes: true,
+  });
+  const EXTENSION = ".html";
 
-    const filesNames = dirents
-        .filter(dirent => dirent.isFile())
-        .map(dirent => dirent.name)
-        .filter(dirent => path.extname(dirent).toLowerCase() === EXTENSION);
+  const filesNames = dirents
+    .filter((dirent) => dirent.isFile())
+    .map((dirent) => dirent.name)
+    .filter((dirent) => path.extname(dirent).toLowerCase() === EXTENSION);
 
-    return filesNames.map(item => {
-        // Split names and extension
-        const parts = item.split('.')
-        const name = parts[0]
-        const extension = parts[1]
-        return new HtmlWebpackPlugin({
-            filename: `${name}.html`,
-            template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-        })
-    })
+  return filesNames.map((item) => {
+    // Split names and extension
+    const parts = item.split(".");
+    const name = parts[0];
+    const extension = parts[1];
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+    });
+  });
 }
 
 // We will call the function like this:
-const htmlPlugins = generateHtmlPlugins('./src');
+const htmlPlugins = generateHtmlPlugins("./src");
 
 module.exports = {
-    entry: './src/index.js',
-    plugins: [
-        new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new CopyPlugin({
-            patterns: [
-                {from: './src/img', to: 'img'}
-            ],
-        }),
-    ].concat(htmlPlugins),
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: "./src/index.js",
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./src/img", to: "img" }],
+    }),
+    new Dotenv(),
+  ].concat(htmlPlugins),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
