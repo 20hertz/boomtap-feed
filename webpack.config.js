e6a3c000face55231a0bbb097e05a1accc6267ce
Stéphane B <stillfury@hotmail.com>
2023-01-23 10:00:47 -0500

    build: rework bundler core config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common.js").default;
+const commonConfig = require("./webpack.common.js");
 const developmentConfig = require("./webpack.dev.js");
 const productionConfig = require("./webpack.prod.js");
 
