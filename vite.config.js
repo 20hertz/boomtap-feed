bd25b78709ba56733d33da9ace254edc04c23153
Stéphane B <stillfury@hotmail.com>
2023-09-15 16:43:28 -0400

    feat: inject environment variables to html

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
-import { defineConfig } from "vite";
+import { loadEnv } from "vite";
 import path from "path";
+import { createHtmlPlugin } from "vite-plugin-html";
 
-export default defineConfig({
-  root: "src",
-  build: {
-    outDir: "../dist",
-  },
-  resolve: {
-    alias: {
-      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    root: "src",
+    build: {
+      outDir: "../dist",
     },
-  },
-});
+    resolve: {
+      alias: {
+        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+      },
+    },
+    plugins: [
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            aweberFormId: env.VITE_AWEBER_FORM_ID,
+            aweberFormName: env.VITE_AWEBER_FORM_NAME,
+            aweberListId: env.VITE_AWEBER_LIST_ID,
+            aweberRedirectUrl: env.VITE_AWEBER_REDIRECT_URL,
+          },
+        },
+      }),
+    ],
+  };
+};
